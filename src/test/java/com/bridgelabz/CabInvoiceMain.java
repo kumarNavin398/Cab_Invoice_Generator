e8a35172c9b53375b8ea0cff01f9d14845f890bb
KumarNavin <navins398@gmail.com>
2023-02-10 06:56:01 +0530

    Multiple_Rides

--- src/test/java/com/bridgelabz/CabInvoiceMain.java
+++ src/test/java/com/bridgelabz/CabInvoiceMain.java
@@ -3,7 +3,17 @@ package com.bridgelabz;
 public class CabInvoiceMain {
     private static final int COST_PER_TIME=1;
     private static final double MIN_COST_PER_KM=10;
-    public double calculateFare(double distance, int time){
-        return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+    private static final double MIN_FARE=5;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public double calculateFare(Ride[] ride1){
+        double  totalFare=0;
+        for (Ride ride:ride1){
+            totalFare+=this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
